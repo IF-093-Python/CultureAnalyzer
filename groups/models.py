90d74babe2f03c46932b1109987a175373cc8854
Jura <jurakulek@gmail.com>
2019-01-16 12:39:27 +0200

    created new model groups

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Groups(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.name}'
