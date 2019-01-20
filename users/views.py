9dbe39e0884172fc1567bf2623ddd21eefc5cc3a
rostIvan <bbwgd77@gmail.com>
2019-01-20 04:27:09 +0200

    Changed base template, added one more abstraction for templates

--- users/views.py
+++ users/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'users/index.html')
+    return render(request, 'users/index.html', context={
+        'is_user_authenticated': True,
+        'username': 'Vasya',
+        'user_role': 'admin'
+    })
