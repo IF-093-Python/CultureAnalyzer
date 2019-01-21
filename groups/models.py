d54d9edba59514bb6acacd8d0562f60142d6bf11
Vladyslav Kolinko <kolinkovlad@gmail.com>
2019-01-21 16:43:10 +0200

    add user relation to Group model

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from users.models import Role
 
 
 class Group(models.Model):
     name = models.CharField(max_length=30)
+    user = models.ManyToManyField(User)
 
     def __str__(self):
         return f'{self.name}'
