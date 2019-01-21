41da61718476b98e103e353ae1e9d41a445b60de
Jura <jurakulek@gmail.com>
2019-01-21 19:05:52 +0200

    [FIX] fixed sign up and sign in views

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(
+    path('login/', views.LoginView.as_view(
         template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         template_name='users/logout.html'), name='logout'),
--- users/views.py
+++ users/views.py
@@ -1,17 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.views.generic import CreateView
+from django.contrib.auth import views as auth_views
 
 
 def index(request):
     return render(request, 'users/index.html')
 
 
+class LoginView(auth_views.LoginView):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super(LoginView, self).get(request, *args, **kwargs)
+
+
 class UserRegisterView(CreateView):
     template_name = 'users/registration.html'
     form_class = UserRegisterForm
     success_url = '/login'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super(UserRegisterView, self).get(request, *args, **kwargs)
 
 
 def profile(request):
