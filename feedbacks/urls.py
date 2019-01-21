45ec569c19b91d6127b4eb1ead7e28dfd1572b79
MrKreg <taras.sheketa@gmail.com>
2019-01-22 01:00:41 +0200

    Created feedback delete viev

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', FeedbackListView.as_view(), name='feedback-list')
+    path('', FeedbackListView.as_view(), name='feedback-list'),
+    path('delete/<int:pk>', FeedbackDeleteView.as_view(), name='feedback-delete'),
 ]
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,7 +1,14 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 from feedbacks.models import Feedback
 
 
 class FeedbackListView(ListView):
     model = Feedback
     paginate_by = 10
+
+
+class FeedbackDeleteView(DeleteView):
+    model = Feedback
+    success_url = reverse_lazy('feedback-list')
