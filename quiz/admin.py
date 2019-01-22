4fa7ef9943369a1eb46172dc397c1bca3c29bafa
Vladyslav Kolinko <kolinkovlad@gmail.com>
2019-01-22 11:26:34 +0200

    delete apps quiz, tutors

--- quiz/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from quiz.models import Quizzes, Results
-admin.site.register(Quizzes)
-admin.site.register(Results)
--- tutors/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TutorsConfig(AppConfig):
-    name = 'tutors'
