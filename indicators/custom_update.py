c442f4e18fb851edd085b27587ccfbb0f1467183
Marian Mandziuk <maryan.mandziuk@gmail.com>
2019-01-26 18:18:15 +0200

    Update comment

--- indicators/custom_update.py
+++ indicators/custom_update.py
@@ -7,11 +7,13 @@ from django.shortcuts import get_object_or_404
 class CustomProcessFormView(ProcessFormView):
 
     def post(self, request, *args, **kwargs):
+        """
+        Update record in model.
 
-        """Update record in model. Delete and create new record with new
-        updated fields. If form isn't valid restore with previous fields
-        record into db."""
-
+        Delete and create new record with new updated fields.
+        If form isn't valid restore with previous fields
+        record into db.
+        """
         model_obj = get_object_or_404(self.model, pk=kwargs.get('pk'))
         tmp_pk = model_obj.pk
         model_obj.delete()
