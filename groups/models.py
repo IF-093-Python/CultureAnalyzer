798cd0d7e44246825cc2775ecc5252362f94e4d3
Adriana Shymoniak <adrianashymoniak@gmail.com>
2019-01-29 23:54:09 +0200

    Fix for groups/models.py and add migrations

--- groups/models.py
+++ groups/models.py
@@ -9,8 +9,8 @@ class Group(models.Model):
         Model for representing Group entity
     """
     name = models.CharField(max_length=30)
-    user = models.ManyToManyField(Profile)
-    mentor = models.ManyToManyField(Profile)
+    user = models.ManyToManyField(Profile, related_name='user_in_group')
+    mentor = models.ManyToManyField(Profile, related_name='mentor_in_group')
 
     def __str__(self):
         return f'{self.name}'
