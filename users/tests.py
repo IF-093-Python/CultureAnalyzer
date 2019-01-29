a31b28e1beb1b3547bf5d0bb270bb38b3c7d94ff
rostIvan <bbwgd77@gmail.com>
2019-01-29 20:41:49 +0200

    Integrated travis with sonarcloud, added djnago tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="lion")
+
+    def test_user(self):
+        lion = User.objects.get(username="lion")
+        self.assertEqual(lion.username, 'lion')
