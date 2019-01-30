2628ca74394b153f8d7b1143c1c93f0013e348e4
Sophie Semi <sofiia.semianchuk@gmail.com>
2019-01-30 20:33:18 +0200

    Fixed  forms.py from tutors_app: added import tutors.models

--- tutors/forms.py
+++ tutors/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Fieldset, Layout, Field, HTML
 from quiz.models import Quizzes
+from tutors.models import Questions, Answers
 from django import forms
 
 
