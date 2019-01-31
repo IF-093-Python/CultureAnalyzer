2a027b8c7b6758705f0f090a001c34defc9b3c8d
Jura <jurakulek@gmail.com>
2019-01-31 23:28:28 +0200

    [FIX] fixed validation for int value

--- users/validators.py
+++ users/validators.py
@@ -4,14 +4,15 @@ MIN_YEARS_OF_EXPERIENCE = 0
 MAX_YEARS_OF_EXPERIENCE = 100
 
 
-class ProfileValidator(object):
+class ProfileValidator:
     @staticmethod
     def validate_experience(rq_data):
         experience = rq_data.get('experience')
 
-        if isinstance(experience, int) and (
-                experience > MAX_YEARS_OF_EXPERIENCE or
-                experience < MIN_YEARS_OF_EXPERIENCE):
+        if not isinstance(experience, int):
+            raise forms.ValidationError('Enter correct data')
+
+        elif not MIN_YEARS_OF_EXPERIENCE < experience < MAX_YEARS_OF_EXPERIENCE:
             raise forms.ValidationError(
                 f'Enter correct data (from {MIN_YEARS_OF_EXPERIENCE} to'
                 f' {MAX_YEARS_OF_EXPERIENCE})')
