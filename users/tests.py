d290c913f512e4256ee6c1966ca44c9c611ae05e
rostIvan <bbwgd77@gmail.com>
2019-02-01 17:53:58 +0200

    Merge branch 'coverage' of https://github.com/IF-093-Python/CultureAnalyzer into 'travis'

--- users/tests.py
+++ users/tests.py
@@ -1,18 +1,27 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
+# noinspection PyUnresolvedReferences
+from .signals import (
+    create_profile,
+    save_profile
+)
+
 
 class UserTestCase(TestCase):
-    fixtures = ('fixtures.json',)
+    fixtures = 'fixtures',
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         User.objects.create(username="john")
         User.objects.create(username="alex")
+        User.objects.create(username="Admin")
 
-    def test_user(self):
-        lion = User.objects.get(username="john")
-        self.assertEqual(lion.username, 'john')
+    def test_john(self):
+        john = User.objects.get(username="john")
+        self.assertEqual(john.username, 'john')
 
-    def test_some(self):
-        lion = User.objects.get(username="alex")
-        self.assertEqual(lion.username, 'alex')
+    def test_alex(self):
+        alex = User.objects.get(username="alex")
+        self.assertEqual(alex.username, 'alex')
