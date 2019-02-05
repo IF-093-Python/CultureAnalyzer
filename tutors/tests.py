6ebb683ffedcff5c19c2e125e5759c92e4a191a0
rostIvan <bbwgd77@gmail.com>
2019-02-05 12:25:04 +0200

    Cleared tutor test

--- tutors/tests.py
+++ tutors/tests.py
@@ -1,22 +1,3 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 
-# noinspection PyUnresolvedReferences
-from users.signals import (
-    create_profile,
-    save_profile
-)
-
-
-class TutorTestCase(TestCase):
-    fixtures = 'fixtures',
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        User.objects.create(username="a")
-
-    def test_temp(self):
-        pass
-
-
+# Tests here
