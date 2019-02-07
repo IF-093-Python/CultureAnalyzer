3cfc29c591071cab2f9dffed46b8c0d7f6e83176
Sophie Semi <sofiia.semianchuk@gmail.com>
2019-02-07 20:16:20 +0200

    Saved answers in session

--- test_player/forms.py
+++ test_player/forms.py
@@ -3,8 +3,10 @@ from django import forms
 
 class QuestionSaveForm(forms.Form):
 
-    def __init__(self, answers, *args, **kwargs):
+    def __init__(self, answers, default_choice, *args, **kwargs):
         super(QuestionSaveForm, self).__init__(*args, **kwargs)
         choice_list = [(answer.id, answer.answer_text) for answer in answers]
         self.fields["answers"] = forms.ChoiceField(choices=choice_list,
-                                                   widget=forms.RadioSelect)
+                                                   widget=forms.RadioSelect,
+                                                   required=False,
+                                                   initial=default_choice)
--- test_player/views.py
+++ test_player/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import FormView
 from .forms import QuestionSaveForm
 from quiz.models import Quizzes
@@ -9,9 +10,15 @@ def index(request):
     return render(request, 'test_player/quizz.html')
 
 
-class TestPlayer():
+class TestPlayer(FormView):
     template_name = 'test_player/test_player.html'
     form_class = QuestionSaveForm
+
+    def get_success_url(self):
+        return reverse_lazy('test_player:detail',
+                            kwargs={'quiz_id': self.kwargs[
+                                'quiz_id'], 'question_title':
+                                self.request.POST.get('next')})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,6 +29,7 @@ class TestPlayer():
                                                             'quiz_id'],
                                                         title=self.kwargs[
                                                             'question_title'])
+
         return context
 
     def get_form_kwargs(self):
@@ -31,6 +39,15 @@ class TestPlayer():
                                               title=self.kwargs[
                                                   'question_title'])
         current_answers = current_questions.answers_set.all()
-        return dict(kwargs, answers=current_answers)
+        if self.kwargs['question_title'] in self.request.session.keys():
+            d_answer = self.request.session[self.kwargs['question_title']]
+        else:
+            d_answer = ''
+        return dict(kwargs, answers=current_answers, default_choice=d_answer)
 
+    def form_valid(self, form):
+        self.request.session[self.kwargs['question_title']] = \
+            form.cleaned_data.get('answers')
+        print(self.request.session.items())
+        return super(TestPlayer, self).form_valid(form)
 
