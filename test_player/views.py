d45f1418f36782bdb0d80db5eab10c16ebb1eb13
Sophie Semi <sofiia.semianchuk@gmail.com>
2019-02-08 12:38:45 +0200

    Modified structure of the session from test_player

--- test_player/views.py
+++ test_player/views.py
@@ -39,14 +39,26 @@ class TestPlayer(FormView):
                                               question_number=self.kwargs[
                                                   'question_number'])
         current_answers = current_questions.answers_set.all()
-        if self.kwargs['question_number'] in self.request.session.keys():
-            d_answer = self.request.session[self.kwargs['question_number']]
+        if self.kwargs['quiz_id'] in self.request.session and self.kwargs[
+            'question_number'] in self.request.session[self.kwargs[
+            'quiz_id']].keys():
+            d_answer = self.request.session[self.kwargs['quiz_id']].get(self.kwargs[
+                        'question_number'])
         else:
-            d_answer = ''
+            d_answer = None
         return dict(kwargs, answers=current_answers, default_choice=d_answer)
 
     def form_valid(self, form):
-        self.request.session[
-            self.kwargs['question_number']] = form.cleaned_data.get('answers')
+        if self.request.session.setdefault(self.kwargs['quiz_id'], False):
+            s = self.request.session[self.kwargs['quiz_id']]
+        else:
+            s = dict.fromkeys(list(zip(*Questions.objects.filter(
+                quiz=self.kwargs['quiz_id']).values_list(
+                'question_number').order_by('question_number')))[0], None)
+            self.request.session[self.kwargs['quiz_id']] = s
+        s.update({
+            self.kwargs['question_number']: form.cleaned_data.get(
+                'answers')})
+        self.request.session[self.kwargs['quiz_id']] = s
         print(self.request.session.items())
         return super(TestPlayer, self).form_valid(form)
