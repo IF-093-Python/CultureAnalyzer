0c83444a78ce1ebf45eb3ee3ad973854753fd301
MrKreg <taras.sheketa@gmail.com>
2019-02-12 20:48:48 +0200

    Changed tests

--- feedbacks/tests.py
+++ feedbacks/tests.py
@@ -1,64 +1,23 @@
-from django.test import TestCase
+from unittest import TestCase
+from ddt import ddt, idata
 
-from feedbacks.models import Feedback, Recommendation
+from feedbacks.test_form_data import FEEDBACK_INVALID_DATA, FEEDBACK_VALID_DATA
 from feedbacks.forms import FeedbackForm
 
 
-class FeedbackTestCase(TestCase):
-    def setUp(self):
-        Feedback.objects.create(feedback='Some useful feedback', min_value=5,
-                                max_value=10, indicator='PDI')
-        Feedback.objects.create(feedback='Some useless feedback', min_value=0,
-                                max_value=15, indicator='IND')
-
-    def test_values(self):
-        useful = Feedback.objects.get(feedback='Some useful feedback')
-        useless = Feedback.objects.get(feedback='Some useless feedback')
-        self.assertEqual(useful.indicator, 'PDI')
-        self.assertEqual(useless.max_value, 15)
+def data_generator(const):
+    for x in const:
+        yield x
 
 
+@ddt
 class FeedbackFormTest(TestCase):
-    def test_valid_form(self):
-        form = FeedbackForm(data={
-            'feedback': 'Some valid feedback',
-            'min_value': 0,
-            'max_value': 10,
-            'indicator': 'PDI',
-        })
+    @idata(data_generator(FEEDBACK_VALID_DATA))
+    def test_valid_form(self, value):
+        form = FeedbackForm(data=value)
         self.assertTrue(form.is_valid())
 
-    def test_invalid_form(self):
-        form = FeedbackForm(data={
-            'feedback': 'Some invalid feedback',
-            'min_value': 0,
-            'max_value': 10,
-            'indicator': 'DSA',
-        })
+    @idata(data_generator(FEEDBACK_INVALID_DATA))
+    def test_invalid_form(self, value):
+        form = FeedbackForm(data=value)
         self.assertFalse(form.is_valid())
-        form = FeedbackForm(data={
-            'feedback': 'Some invalid feedback',
-            'min_value': 20,
-            'max_value': 10,
-            'indicator': 'PDI',
-        })
-        self.assertFalse(form.is_valid())
-
-
-class RecommendationTestCase(TestCase):
-    def setUp(self):
-        Feedback.objects.create(feedback='Some useful feedback', min_value=5,
-                                max_value=10, indicator='PDI')
-        Recommendation.objects.create(
-            recommendation='It is a good advice',
-            feedback=Feedback.objects.get(feedback='Some useful feedback'))
-        Recommendation.objects.create(
-            recommendation='It is a bad advice',
-            feedback=Feedback.objects.get(feedback='Some useful feedback'))
-
-    def test_values(self):
-        good = Recommendation.objects.get(recommendation='It is a good advice')
-        bad = Recommendation.objects.get(recommendation='It is a bad advice')
-        feedback = Feedback.objects.get(feedback='Some useful feedback')
-        self.assertEqual(good.recommendation, 'It is a good advice')
-        self.assertEqual(bad.feedback.feedback, feedback.feedback)
--- feedbacks/validator.py
+++ feedbacks/validator.py
@@ -3,6 +3,6 @@ class FeedbackValidator:
     def validate_min_value(rq_data):
         min_value = rq_data.cleaned_data.get('min_value')
         max_value = rq_data.cleaned_data.get('max_value')
-        if min_value > max_value:
+        if min_value >= max_value:
             rq_data.add_error('min_value', 'Min value must be less then max value')
         return min_value
