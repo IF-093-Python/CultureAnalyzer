241462b532ff07c0fe3ea63c5f2f5ede66a78cb8
rostIvan <bbwgd77@gmail.com>
2019-02-12 16:57:50 +0200

    Added base user API methods with permissions

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
 
-from .views import protected_view
+from api import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+
     path('token/', include([
         path('obtain/', TokenObtainPairView.as_view()),
         path('refresh/', TokenRefreshView.as_view()),
     ])),
 
-    path('protected/', protected_view)
+    path('protected/', views.protected_view)
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,34 @@
-from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.permissions import IsAdmin, IsMentor, IsSuperAdmin
+from api.permissions import is_mentor, is_admin, is_superadmin
+from api.service import get_superadmin_users, get_mentor_users, get_admin_users
+from .serializers import UserSerializer
 
 
 @api_view(['GET'])
 def protected_view(request):
     return Response({'username': f'{request.user.username}'})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsSuperAdmin | IsAdmin | IsMentor,)
+    queryset = User.objects.none()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return self.query_by_role(user)
+
+    def query_by_role(self, user):
+        queryset = super(UserViewSet, self).get_queryset()
+        if is_superadmin(user):
+            queryset = get_superadmin_users()
+        elif is_admin(user):
+            queryset = get_admin_users()
+        elif is_mentor(user):
+            queryset = get_mentor_users()
+        return queryset
