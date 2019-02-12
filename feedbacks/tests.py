3a124c8b5e2533fc81d0e8b4f7c4229ef2a6e3e2
MrKreg <taras.sheketa@gmail.com>
2019-02-13 00:00:15 +0200

    Added feedback list view test case

--- feedbacks/tests.py
+++ feedbacks/tests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from ddt import ddt, idata
+from django.test import TestCase as DjangoTestCase
+from django.contrib.auth.models import User
 
 from feedbacks.test_form_data import FEEDBACK_INVALID_DATA, FEEDBACK_VALID_DATA
 from feedbacks.forms import FeedbackForm
@@ -7,19 +9,38 @@ from feedbacks.forms import FeedbackForm
 __all__ = ['FeedbackFormTest', ]
 
 
-def data_generator(const):
-    for x in const:
+def form_data_generator(data):
+    for x in data:
         yield x
 
 
 @ddt
 class FeedbackFormTest(TestCase):
-    @idata(data_generator(FEEDBACK_VALID_DATA))
+    @idata(form_data_generator(FEEDBACK_VALID_DATA))
     def test_valid_form(self, value):
         form = FeedbackForm(data=value)
         self.assertTrue(form.is_valid())
 
-    @idata(data_generator(FEEDBACK_INVALID_DATA))
+    @idata(form_data_generator(FEEDBACK_INVALID_DATA))
     def test_invalid_form(self, value):
         form = FeedbackForm(data=value)
         self.assertFalse(form.is_valid())
+
+
+class FeedbackListViewTest(DjangoTestCase):
+    fixtures = ('users/fixtures/fixtures.json',)
+
+    def setUp(self):
+        User.objects.create_user('user', password='test').save()
+
+    def test_call_view_denies_anonymous(self):
+        response = self.client.get('/feedbacks/', follow=True)
+        self.assertRedirects(response, '/login/?next=/feedbacks/')
+        response = self.client.post('/feedbacks/', follow=True)
+        self.assertRedirects(response, '/login/?next=/feedbacks/')
+
+    def test_call_view_loads(self):
+        self.client.login(username='user', password='test')
+        response = self.client.get('/feedbacks/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'feedbacks/feedback_list.html')
