c68e840111db4640c0136af17a262f59829e0ebe
rostIvan <bbwgd77@gmail.com>
2019-02-12 23:51:20 +0200

    Added encrypted deploy key and preparation commands to deploy

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,24 @@
+import os
 from fabric import task
 from paramiko import SSHClient
+
+env = os.environ
 
 
 @task
 def deploy(ctx):
-    print('Running...')
+    print('-> Run fab deploy <-')
+    ssh_client = SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.connect(username=env['DEPLOY_USERNAME'],
+                       hostname=env['DEPLOY_HOSTNAME'],
+                       port=env['DEPLOY_PORT'],
+                       key_filename=env['DEPLOY_KEY_FILENAME'])
+    stdin, stdout, stderr = ssh_client.exec_command(env['DEPLOY_COMMAND'])
+    print(get_output(stderr, stdout))
+    ssh_client.close()
+    print('-> End deploy <-')
+
+
+def get_output(stderr, stdout):
+    return 'out: ' + str(stdout.read()) + '\nerr: ' + str(stderr.read())
