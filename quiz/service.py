d75653ed6d7fa9b49192127d2061ee67b7036caa
Jura <jurakulek@gmail.com>
2019-02-14 18:44:09 +0200

    add final result func

--- quiz/service.py
+++ quiz/service.py
@@ -1,3 +1,8 @@
+import json
+
+from users.models import Profile
+
+
 def get_constant(indicator_value):
     """
     Return constant for wrong indicator
@@ -95,15 +100,52 @@ def get_indicators_values(answers_list):
     :return: dict dictionary with value for each indicator
     """
     group = {'pdi': round(35 * (answers_list[6] - answers_list[1]) + 25 * (
-            answers_list[19] - answers_list[22]), 2),
-             'idv': round(35 * (answers_list[3] - answers_list[0]) + 35 * (
-                     answers_list[8] - answers_list[5]), 2),
-             'mas': round(35 * (answers_list[4] - answers_list[2]) + 35 * (
-                     answers_list[7] - answers_list[9]), 2),
-             'uai': round(40 * (answers_list[17] - answers_list[14]) + 25 * (
-                     answers_list[20] - answers_list[23]), 2),
-             'lto': round(40 * (answers_list[12] - answers_list[13]) + 25 * (
-                     answers_list[18] - answers_list[21]), 2),
-             'ivr': round(35 * (answers_list[11] - answers_list[10]) + 40 * (
-                     answers_list[16] - answers_list[15]), 2)}
+        answers_list[19] - answers_list[22]), 2),
+        'idv': round(35 * (answers_list[3] - answers_list[0]) + 35 * (
+            answers_list[8] - answers_list[5]), 2),
+        'mas': round(35 * (answers_list[4] - answers_list[2]) + 35 * (
+            answers_list[7] - answers_list[9]), 2),
+        'uai': round(40 * (answers_list[17] - answers_list[14]) + 25 * (
+            answers_list[20] - answers_list[23]), 2),
+        'lto': round(40 * (answers_list[12] - answers_list[13]) + 25 * (
+            answers_list[18] - answers_list[21]), 2),
+        'ivr': round(35 * (answers_list[11] - answers_list[10]) + 40 * (
+            answers_list[16] - answers_list[15]), 2)}
     return group
+
+
+def get_groups_results(data):
+    """
+    Return list with list of results
+
+    :param list data: List with users profile
+    :return: List with lists of results
+    """
+    list_of_results = []
+    for result in data:
+        list_of_results.append(json.loads(result.user.results_set.last().result))
+
+    return list_of_results
+
+
+def get_final_result(data, *args):
+    if isinstance(data, Profile):
+        group_answers = [json.loads(data.user.results_set.filter(pk=args[0]).first().result)]
+    else:
+        users_results = data.user.all().exclude(user__results=None)
+        if not users_results:
+            return {
+                'pdi': 0,
+                'idv': 0,
+                'mas': 0,
+                'uai': 0,
+                'lto': 0,
+                'ivr': 0,
+            }
+        group_answers = get_groups_results(users_results)
+    results_list = get_list_of_results(group_answers)
+    average_result = get_average_results(results_list)
+    indicator_list = get_indicators_values(average_result)
+    data = check_group_indicators(indicator_list)
+
+    return data
