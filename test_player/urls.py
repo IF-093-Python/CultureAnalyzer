679a52f6cba79333e13c216001011783467455eb
Vladyslav Kolinko <kolinkovlad@gmail.com>
2019-02-15 12:30:15 +0200

    add results list

--- test_player/urls.py
+++ test_player/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path
-from .views import TestPlayer, TestStart
+
+from .views import TestPlayer, TestStart, ResultsListView, ResultDetailView
 
 app_name = 'test_player'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', TestStart.as_view(), name='start_test'),
     re_path(r'^(?P<quiz_id>[0-9]+)/(?P<question_number>[0-9]+)/',
             TestPlayer.as_view(), name='test_player'),
+    path('results/', ResultsListView.as_view(), name='results-list'),
+    path('<int:pk>', ResultDetailView.as_view(), name='result-detail'),
 ]
