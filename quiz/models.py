89eb4d47662e642ec4359639e93d8732a8f53e4a
Jura <jurakulek@gmail.com>
2019-02-18 19:33:30 +0200

    change user model

--- quiz/models.py
+++ quiz/models.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import CASCADE
 from quiz.choices import TYPE_OF_QUIZ
+
+from users.models import CustomUser
 
 
 class Quizzes(models.Model):
@@ -14,7 +15,7 @@ class Quizzes(models.Model):
 
 
 class Results(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE, null=False)
+    user = models.ForeignKey(CustomUser, on_delete=CASCADE, null=False)
     quiz = models.ForeignKey(Quizzes, on_delete=CASCADE, null=False)
     pass_date = models.DateTimeField(null=False)
     result = models.TextField('Description', null=False)
