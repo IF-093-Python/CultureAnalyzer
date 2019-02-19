baeea5be4fb0d1bd26336129a4917dfc6ca13f88
Yarema Kryven <brebeneskyl@gmail.com>
2019-02-19 11:12:54 +0200

    Added django_bootstrap_breadcrumbs

--- groups/views.py
+++ groups/views.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from groups.forms import GroupCreateForm,GroupUpdateForm
 from groups.models import Group
 from django.contrib.messages.views import SuccessMessageMixin
-from users.models import User
+from users.models import User,Profile
 from django.shortcuts import get_object_or_404
 from itertools import chain
 
@@ -90,18 +90,25 @@ class UpdateGroupView(SuccessMessageMixin,
         return context
 
     def get_queryset(self):
+        """Remembers all checked_mentors of group and then
+        concatenates them with unchecked metors, so that
+        checked mentors are always first in list"""
         checked_mentors=User.objects.filter(
             profile__mentor_in_group=self.kwargs['pk']).\
             filter(is_active=True).order_by('last_name')
         self.__checked_mentors=checked_mentors
-        result = User.objects.filter(is_active=True).\
+        mentors = User.objects.filter(is_active=True).exclude(
+            profile__mentor_in_group=self.kwargs['pk']). \
             filter(profile__role__name='Mentor').order_by('last_name')
         if self.request.GET.get('data_search'):
-            result = result.filter(
+            mentors = mentors.filter(
+                last_name__contains=self.request.GET.get('data_search'))
+            checked_mentors=checked_mentors.filter(
                 last_name__contains=self.request.GET.get('data_search'))
             self.__search = True
             self.paginate_by = None
             self.__search_label = self.request.GET.get('data_search')
+        result = list(chain(checked_mentors, mentors))
         return result
 
     def get_object(self,context=None):
@@ -190,7 +197,6 @@ class MentorGroupAdd(SuccessMessageMixin,LoginRequiredMixin,
     success_message = "Group was updated successfully"
     __search = False
     __search_label = 'Search'
-    __users_in_group = None
     paginate_by = PAGINATOR
 
     def get_success_url(self):
@@ -201,13 +207,11 @@ class MentorGroupAdd(SuccessMessageMixin,LoginRequiredMixin,
         context = super().get_context_data(**kwargs)
         context['search_label'] = self.__search_label
         context['search'] = self.__search
-        context['added_users'] = self.__users_in_group
         return context
 
     def get_queryset(self):
-        users_in_group = User.objects.filter(is_active=True). \
-            filter(profile__user_in_group=self.kwargs['pk'])
-        self.__users_in_group=users_in_group
+        '''Gets all Trainee users that are not in groups and
+        makes search in their last_name if needed'''
         result = User.objects.filter(profile__role__name='Trainee').\
             exclude(profile__user_in_group=self.kwargs['pk']).\
             filter(is_active=True).\
@@ -223,3 +227,13 @@ class MentorGroupAdd(SuccessMessageMixin,LoginRequiredMixin,
     def get_object(self,context=None):
         context = get_object_or_404(Group, pk=self.kwargs['pk'])
         return context
+
+    def form_valid(self, form):
+        '''Gets users that are already in group and adds to users
+        that where checked in form for adding to group'''
+        users_in_group = Profile.objects.filter(user__is_active=True).\
+            filter(user_in_group=self.kwargs['pk'])
+        form.cleaned_data['user']=\
+            chain(form.cleaned_data['user'], users_in_group)
+        return super(MentorGroupAdd,self).form_valid(form)
+
