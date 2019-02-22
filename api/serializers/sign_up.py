fe089e21cb74ff990ab6c19d2ab87dcbea56ff34
rostIvan <bbwgd77@gmail.com>
2019-02-22 12:52:52 +0200

    Like stash

---
+++ api/serializers/sign_up.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from api.validators import validate_sign_up
+from users.models import CustomUser
+
+__all__ = ['SignUpSerializer']
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ("id", "username", "password", 'email',)
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = self.create_user(username=validated_data['username'],
+                                email=validated_data['email'],
+                                password=validated_data['password'])
+        return user
+
+    def create_user(self, username, email, password):
+        user = CustomUser.objects.create(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def validate(self, attrs):
+        validate_sign_up(attrs)
+        return super().validate(attrs)
