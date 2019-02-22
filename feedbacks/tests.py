fecd2deaf2f834d8ec20512449154d5885847b48
Jura <jurakulek@gmail.com>
2019-02-22 18:47:13 +0200

    [FIX] fixed tests

--- feedbacks/tests.py
+++ feedbacks/tests.py
@@ -1,13 +1,13 @@
 from unittest import TestCase
 from ddt import ddt, idata, data
 from django.test import TestCase as DjangoTestCase
-from django.contrib.auth.models import User
 
 from feedbacks.tests_data.test_form_data import (FEEDBACK_INVALID_DATA,
                                                  FEEDBACK_VALID_DATA)
 from feedbacks.tests_data.test_view_data import PAGE_STRING_VALUES
 from feedbacks.models import Feedback
 from feedbacks.forms import FeedbackForm
+from users.models import CustomUser
 
 __all__ = ['FeedbackFormTest', ]
 
@@ -28,7 +28,7 @@ class FeedbackFormTest(TestCase):
 @ddt
 class FeedbackListViewTest(DjangoTestCase):
     def setUp(self):
-        User.objects.create_user('user', password='test').save()
+        CustomUser.objects.create_user('user', password='test').save()
         for i in range(10):
             Feedback.objects.create(feedback='Some text', min_value=i + 1,
                                     max_value=i + 5, indicator='PDI')
