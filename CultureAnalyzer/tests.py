45bccaa06de10a3bf98b02bb9d06b38a3f2d6001
Jura <jurakulek@gmail.com>
2019-02-25 11:51:18 +0200

    [FIX] fixed test

--- CultureAnalyzer/tests.py
+++ CultureAnalyzer/tests.py
@@ -6,4 +6,5 @@ class CustomTestRunner(DiscoverRunner):
     def setup_databases(self, **kwargs):
         databases = super().setup_databases(**kwargs)
         call_command("loaddata", "users/fixtures/fixtures.json")
+        call_command("loaddata", "users/fixtures/fixtures_groups.json")
         return databases
