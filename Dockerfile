fe52da73634634e58afb504234d8c45f7da026df
rostIvan <bbwgd77@gmail.com>
2019-03-02 19:27:08 +0200

    Dockerize GCP

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.6
+
+RUN mkdir -p /opt/services/CultureAnalyzer
+WORKDIR /opt/services/CultureAnalyzer
+
+COPY . /opt/services/CultureAnalyzer
+
+EXPOSE 8000
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate --no-input
+python manage.py loaddata users/fixtures/fixtures.json
+mkdir logs
+touch ./logs/gunicorn.log
+touch ./logs/gunicorn-access.log
+tail -n 0 -f ./logs/gunicorn*.log &
+
+gunicorn --bind :8000 CultureAnalyzer.wsgi:application --reload --log-level=info --log-file=./logs/gunicorn.log --access-logfile=./logs/gunicorn-access.log
+
+exec "$@"
