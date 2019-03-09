f399cddd41285cf89f3b0361a974c110c6e5056c
MrKreg <taras.sheketa@gmail.com>
2019-03-09 04:21:30 +0200

    Added tests for recommendation model

--- feedbacks/tests/test_models.py
+++ feedbacks/tests/test_models.py
@@ -27,3 +27,24 @@ class FeedbackModelTest(TestCase):
             Feedback.objects.create(feedback='Some feedback with the same data',
                                     min_value=10, max_value=20, indicator='PDI')
         self.assertTrue(isinstance(cm.exception, IntegrityError))
+
+
+class RecommendationModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        feedaback = Feedback.objects.create(feedback='Some useful feedback',
+                                            min_value=10,
+                                            max_value=20, indicator='PDI')
+        Recommendation.objects.create(feedback=feedaback,
+                                      recommendation='Some recommendation')
+
+    def test_get_absolute_url(self):
+        recommendation = Recommendation.objects.get(pk=1)
+        self.assertEquals(recommendation.get_absolute_url(),
+                          f'/feedbacks/{recommendation.feedback.pk}')
+
+    def test_object_name_is_first_30_characters_of_recommendation(self):
+        recommendation = Recommendation.objects.get(pk=1)
+        expected_object_name = recommendation.recommendation[:30]
+        self.assertEquals(expected_object_name, str(recommendation))
