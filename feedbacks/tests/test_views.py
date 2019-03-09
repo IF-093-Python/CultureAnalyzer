8b5b94563f4b118df4836992f8dddd9cbee9c23d
MrKreg <taras.sheketa@gmail.com>
2019-03-09 17:49:04 +0200

    Changed from magic strings with route to reverse method

--- feedbacks/tests/test_views.py
+++ feedbacks/tests/test_views.py
@@ -1,58 +1,64 @@
-# from unittest import TestCase
-# from ddt import ddt, idata, data
-# from django.test import TestCase as DjangoTestCase
-# from django.contrib.auth.models import User
-#
-# from feedbacks.tests_data.test_form_data import (FEEDBACK_INVALID_DATA,
-#                                                  FEEDBACK_VALID_DATA)
-# from feedbacks.tests_data.test_view_data import PAGE_STRING_VALUES
-# from feedbacks.models import Feedback
-# from feedbacks.forms import FeedbackForm
-#
-# __all__ = ['FeedbackFormTest', ]
-#
-#
-# @ddt
-# class FeedbackFormTest(TestCase):
-#     @data(*FEEDBACK_VALID_DATA)
-#     def test_valid_form(self, value):
-#         form = FeedbackForm(data=value)
-#         self.assertTrue(form.is_valid())
-#
-#     @data(*FEEDBACK_INVALID_DATA)
-#     def test_invalid_form(self, value):
-#         form = FeedbackForm(data=value)
-#         self.assertFalse(form.is_valid())
-#
-#
-# @ddt
-# class FeedbackListViewTest(DjangoTestCase):
-#     def setUp(self):
-#         User.objects.create_user('user', password='test').save()
-#         for i in range(10):
-#             Feedback.objects.create(feedback='Some text', min_value=i + 1,
-#                                     max_value=i + 5, indicator='PDI')
-#
-#     def test_call_view_denies_anonymous(self):
-#         response = self.client.get('/feedbacks/', follow=True)
-#         self.assertRedirects(response, '/login/?next=/feedbacks/')
-#         response = self.client.post('/feedbacks/', follow=True)
-#         self.assertRedirects(response, '/login/?next=/feedbacks/')
-#
-#     def test_call_view_loads(self):
-#         self.client.login(username='user', password='test')
-#         response = self.client.get('/feedbacks/')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'feedbacks/feedback_list.html')
-#
-#     @data(*range(-10, 10))
-#     def test_call_view_where_page_number_int(self, page):
-#         self.client.login(username='user', password='test')
-#         response = self.client.get(f'/feedbacks/?page={page}')
-#         self.assertEqual(response.status_code, 200)
-#
-#     @data(*PAGE_STRING_VALUES)
-#     def test_call_view_where_page_number_not_int(self, page):
-#         self.client.login(username='user', password='test')
-#         response = self.client.get(f'/feedbacks/?page={page}')
-#         self.assertEqual(response.status_code, 200)
+from ddt import ddt, data
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from feedbacks.tests_data.test_view_data import PAGE_STRING_VALUES
+from feedbacks.models import Feedback
+
+__all__ = ['FeedbackListViewTest', ]
+
+
+@ddt
+class FeedbackListViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user('user', password='test')
+        for i in range(10):
+            Feedback.objects.create(feedback='Some text', min_value=i,
+                                    max_value=i + 5, indicator='PDI')
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('feedback-list'), follow=True)
+        self.assertRedirects(response,
+                             f'/login/?next={reverse("feedback-list")}')
+        response = self.client.post(reverse('feedback-list'), follow=True)
+        self.assertRedirects(response,
+                             f'/login/?next={reverse("feedback-list")}')
+
+    def test_call_view_loads(self):
+        self.client.login(username='user', password='test')
+        response = self.client.get(reverse('feedback-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'feedbacks/feedback_list.html')
+
+    @data(*range(-10, 1))
+    def test_call_view_where_page_number_less_then_1(self, page):
+        self.client.login(username='user', password='test')
+        response = self.client.get(f'{reverse("feedback-list")}?page={page}')
+        expected_response = self.client.get(
+            f'{reverse("feedback-list")}?page=1')
+        self.assertEqual(response.status_code, expected_response.status_code)
+        self.assertEqual(response.rendered_content,
+                         expected_response.rendered_content)
+
+    @data(*range(10, 21))
+    def test_call_view_where_page_number_is_to_large(self, page):
+        self.client.login(username='user', password='test')
+        response = self.client.get(f'{reverse("feedback-list")}?page={page}')
+        expected_response = self.client.get(
+            f'{reverse("feedback-list")}?page=2')
+        self.assertEqual(response.status_code, expected_response.status_code)
+        self.assertEqual(response.rendered_content,
+                         expected_response.rendered_content)
+
+    @data(*PAGE_STRING_VALUES)
+    def test_call_view_where_page_number_not_int(self, page):
+        self.client.login(username='user', password='test')
+        response = self.client.get(f'{reverse("feedback-list")}?page={page}')
+        expected_response = self.client.get(
+            f'{reverse("feedback-list")}?page=1')
+        self.assertEqual(response.status_code, expected_response.status_code)
+        self.assertEqual(response.rendered_content,
+                         expected_response.rendered_content)
