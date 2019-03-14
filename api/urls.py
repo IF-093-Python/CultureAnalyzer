c6c5df28230917778799f5cfae34d97266f12e67
Rostislav Ivankiv <bbwgd77@gmail.com>
2019-03-14 14:29:42 +0200

    Added base sign-up via API (#44)

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
+from api.views import SignUpView
 
 urlpatterns = [
     path('token/', include([
         path('obtain/', TokenObtainPairView.as_view()),
         path('refresh/', TokenRefreshView.as_view()),
     ])),
+    path('sign-up/', SignUpView.as_view()),
 ]
