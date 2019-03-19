b7d965606aa0917f21b489f35073677de03af989
rostIvan <bbwgd77@gmail.com>
2019-03-19 19:35:52 +0200

    Changed AccountSerializer from mixin to class inherited from ModelSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,33 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
-from api.mixins import AccountSerializerMixin
+from api.fields import PasswordField, UniqueEmailField
 
 __all__ = ['SignUpSerializer', 'ProfileSerializer']
 
 
-class SignUpSerializer(AccountSerializerMixin, serializers.ModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
+    password = PasswordField()
+    email = UniqueEmailField()
+
+    @property
+    def validated_data(self):
+        validated_data = super().validated_data
+        password = validated_data.get('password')
+        if password:
+            validated_data['password'] = make_password(password)
+        return validated_data
+
+
+class SignUpSerializer(AccountSerializer):
     class Meta:
         model = get_user_model()
         fields = ('username', 'password', 'email',
                   'first_name', 'last_name')
 
 
-class ProfileSerializer(AccountSerializerMixin, serializers.ModelSerializer):
+class ProfileSerializer(AccountSerializer):
     class Meta:
         model = get_user_model()
         fields = ('username', 'password', 'email',
