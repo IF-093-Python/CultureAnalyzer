28d0fee856d731d8f8e45db750214f402706fdd9
Jura <jurakulek@gmail.com>
2019-03-20 19:46:38 +0200

    add docstrings for errors

--- CultureAnalyzer/exceptions.py
+++ CultureAnalyzer/exceptions.py
@@ -2,12 +2,15 @@ __all__ = ['FValidationError', 'PValidationError']
 
 
 class CAError(Exception):
+    """Custom parent exception"""
     pass
 
 
 class FValidationError(CAError, ValueError):
+    """Feedback validation has some errors"""
     pass
 
 
 class PValidationError(CAError, ValueError):
+    """Profile validation has some errors"""
     pass
