6eadc72e7c0dd98a464418ab2a143dcc8e271efd
Vladyslav Kolinko <kolinkovlad@gmail.com>
2019-03-25 21:03:03 +0200

    fixed mentor-related views

--- api/views.py
+++ api/views.py
@@ -6,14 +6,22 @@ from rest_framework.mixins import (CreateModelMixin, UpdateModelMixin,
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
-from api.serializers import (SignUpSerializer, FeedbackSerializer,
-                             ProfileSerializer, TraineeQuizzesSerializer,
-                             PermissionGroup)
+from api.serializers.account import SignUpSerializer, ProfileSerializer
+from api.serializers.country_indicator import CountryIndicatorSerializer
+from api.serializers.feedback import FeedbackSerializer
+from api.serializers.permissions_group import PermissionGroupSerializer
+from api.serializers.quiz import (MentorQuizSerializer,
+                                  MentorQuestionSerializer,
+                                  MentorAnswerSerializer,
+                                  TraineeQuizzesSerializer)
 from feedbacks.models import Feedback
+from indicators.models import CountryIndicator
 from quiz.models import Quizzes
+from tutors.models import Questions, Answers
 
 __all__ = ['SignUpView', 'ProfileView', 'FeedbackViewSet',
-           'TraineeQuizzesView', 'GroupViewSet']
+           'TraineeQuizzesView', 'GroupViewSet', 'CountryIndicatorViewSet',
+           'MentorQuizViewSet', 'MentorQuestionViewSet', 'MentorAnswerViewSet']
 
 
 class SignUpView(generics.CreateAPIView):
@@ -46,4 +54,38 @@ class TraineeQuizzesView(generics.ListAPIView):
 class GroupViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,
                    ListModelMixin, GenericViewSet):
     queryset = Group.objects.all()
-    serializer_class = PermissionGroup
+    serializer_class = PermissionGroupSerializer
+
+
+class CountryIndicatorViewSet(viewsets.ModelViewSet):
+    queryset = CountryIndicator.objects.all()
+    serializer_class = CountryIndicatorSerializer
+    filter_fields = (
+        'iso_code', 'name', 'pdi', 'idv', 'mas', 'uai', 'lto', 'ivr')
+    search_fields = (
+        'iso_code', 'name', 'pdi', 'idv', 'mas', 'uai', 'lto', 'ivr')
+
+
+class MentorQuizViewSet(viewsets.ModelViewSet):
+    queryset = Quizzes.objects.all()
+    serializer_class = MentorQuizSerializer
+    filter_fields = ('id', 'title', 'description', 'type_of_quiz')
+    search_fields = ('id', 'title', 'description', 'type_of_quiz')
+
+
+class MentorQuestionViewSet(viewsets.ModelViewSet):
+    queryset = Questions.objects.all()
+    serializer_class = MentorQuestionSerializer
+    filterset_fields = (
+        'quiz__id', 'quiz__title', 'quiz__type_of_quiz', 'question_number')
+    search_fields = {
+        'question_number': ('exact',),
+        'question_text': ('icontains',)}
+
+
+class MentorAnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answers.objects.all()
+    serializer_class = MentorAnswerSerializer
+    filter_fields = ('question__id', 'question__id',
+                     'question__question_number' 'answer_number',)
+    search_fields = {'answer_text': 'icontains'}
