3e266c4c58af7a4aa7cdf2c0700d91df902013c4
YaremaKryven <46071963+Yarema-Kryven@users.noreply.github.com>
2019-03-25 15:41:16 +0200

    Groups (#79)
    * Added models and forms for Groups, Shedule and Invitation
    
    * Added validator for Invitation and Shedule forms.
    
    * Added Groups CRUD views for admin
    
    * Changed validators for SheduleForm and InvitationForm
    
    * Changed urls to hyphens
    
    * Added django-bootstrap-datepicker to apps and requirements
    
    * 1)Changed Pagination to constants/ITEMS_ON_PAGE
    2)Changed hardcoded groups_name='Mentor' to groups_pk=constants/MENTOR_ID
    
    * 1)Changed validation in SheduleForm from clean to clean_fields
    2)Changed i tags to em in templates/groups/groups_list
    3)Changed * in groups/urls to explicit naming
    
    * Added CRUD for Mentor_Groups, Invitation and Shedule
    
    * changed mentor_groups_list for work without group-chart
    
    * Fixed bugs for pylint check in templates

--- groups/urls.py
+++ groups/urls.py
@@ -1,15 +1,32 @@
 from django.urls import path
 from groups.views import GroupsList, CreateGroupView, UpdateGroupView, \
-    DeleteGroupView
+    DeleteGroupView, MentorGroupsView, MentorGroupUpdate, MentorGroupAdd, \
+    AddNewUser, SheduleGroupListView, SheduleGroupView, AddInvitation
 
 app_name = 'groups'
 
 urlpatterns = [
-    path('', GroupsList.as_view(), name='groups-list'),
-    path('create-group/', CreateGroupView.as_view(), name='create-group'),
+    path('', GroupsList.as_view(),
+         name='groups-list'),
+    path('create-group/', CreateGroupView.as_view(),
+         name='create-group'),
     path('delete-group/<int:pk>', DeleteGroupView.as_view(),
          name='delete-group'),
     path('update-group/<int:pk>', UpdateGroupView.as_view(),
          name='update-group'),
+    path('mentor-groups/', MentorGroupsView.as_view(),
+         name='mentor_groups_view'),
+    path('group/<int:pk>/', MentorGroupUpdate.as_view(),
+         name='mentor_group_update'),
+    path('group/add/<int:pk>/', MentorGroupAdd.as_view(),
+         name='mentor_group_add'),
+    path('invite/<int:pk>/', AddInvitation.as_view(),
+         name='add_invitation'),
+    path('join/<str:hash>', AddNewUser.as_view(),
+         name='add_new_user'),
+    path('group/set_quiz/<int:pk>/', SheduleGroupView.as_view(),
+         name='shedule_group'),
+    path('group/quiz/<int:pk>/', SheduleGroupListView.as_view(),
+         name='shedule_group_list'),
 
 ]
