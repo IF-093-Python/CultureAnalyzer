64143a279781a90431582aa812815efbdc451c73
yukish <iwan.jukisch@gmail.com>
2019-03-25 12:57:19 +0200

    permissions

--- api/permissions.py
+++ api/permissions.py
@@ -2,23 +2,28 @@ from django.contrib.auth.models import Group
 from rest_framework.permissions import BasePermission
 
 
-def is_in_group(user, group_id):
-    try:
-        print(Group.objects.get(id=group_id).user_set.filter(
-            id=user.id))
-        return Group.objects.get(id=group_id).user_set.filter(
-            id=user.id).exists()
-    except Group.DoesNotExist:
-        return False
-
-
 class HasGroupPermission(BasePermission):
-    def has_permission(self, request, view):
-        required_groups = view.permission_groups.get(view.action)
-        if required_groups is None:
+    @staticmethod
+    def _is_in_group(user, group_id):
+        '''
+
+        :param user:
+        :param group_id:
+        :return:
+        '''
+        try:
+            return Group.objects.get(id=group_id).user_set.filter(
+                id=user.id).exists()
+        except Group.DoesNotExist:
             return False
-        elif '_Public' in required_groups:
+
+    def has_permission(self, request, view):
+        _required_groups = view.permission_groups.get(view.action)
+        if request.user.is_superuser:
             return True
+        elif _required_groups is None:
+            return False
         else:
-            return any([is_in_group(request.user, group_id) for group_id in
-                        required_groups])
+            return any(
+                [self._is_in_group(request.user, group_id) for group_id in
+                 _required_groups])
