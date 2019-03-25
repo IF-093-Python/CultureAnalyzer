9402d4127b21b849386ee3dfe4293a92872b7aa3
Vladyslav Kolinko <kolinkovlad@gmail.com>
2019-03-25 13:38:11 +0200

    Test player app.

---
+++ test_player/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+
+from test_player.views import TestPlayer, StartTest
+
+app_name = 'test_player'
+
+urlpatterns = [
+
+    path('', StartTest.as_view(), name='start_test'),
+    re_path(r'^(?P<quiz_id>[0-9]+)/(?P<question_number>[0-9]+)/',
+            TestPlayer.as_view(), name='test_player'),
+]
