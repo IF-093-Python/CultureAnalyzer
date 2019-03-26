1a30b9e36c634e3ed13b3c97eb00675c9f4bd1b5
Vladyslav Kolinko <kolinkovlad@gmail.com>
2019-03-26 23:38:31 +0200

    fixed middleware

--- CultureAnalyzer/middlewares.py
+++ CultureAnalyzer/middlewares.py
@@ -43,8 +43,11 @@ class SwitchSessionDataMiddleware:
                 self.switch_session_data(request, current_session_key,
                                          stored_session_key)
 
+            # update LoggedInUser table with relevant session key
+            request.user.logged_in_user.session_key = current_session_key
+            request.user.logged_in_user.save()
+
     @staticmethod
-    @transaction.atomic()
     def switch_session_data(request, current_session_key,
                             stored_session_key):
         """
@@ -64,12 +67,11 @@ class SwitchSessionDataMiddleware:
             session_key=stored_session_key).session_data
         # remove not used anymore session
         Session.objects.get(session_key=stored_session_key).delete()
-        # update current session
+
         expire_date = request.session.get_expiry_date()
-        Session.objects.update(
-            session_key=current_session_key,
-            session_data=stored_session_data,
-            expire_date=expire_date)
-        # update LoggedInUser table with relevant session key
-        request.user.logged_in_user.session_key = current_session_key
-        request.user.logged_in_user.save()
+
+        # update current session
+        session_object = Session.objects.get(session_key=current_session_key)
+        session_object.session_data = stored_session_data
+        session_object.expire_date = expire_date
+        session_object.save()
