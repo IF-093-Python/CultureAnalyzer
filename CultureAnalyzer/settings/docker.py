2395072e1a6297f65ebc53e10ea74240f051392d
rostIvan <bbwgd77@gmail.com>
2019-04-04 04:10:34 +0300

    Added .env vars for postgres container

--- CultureAnalyzer/settings/docker.py
+++ CultureAnalyzer/settings/docker.py
@@ -1,14 +1,18 @@
+import os
+
 ALLOWED_HOSTS = ['*']
 
 STATICFILES_DIRS = []
 STATIC_ROOT = "/opt/services/CultureAnalyzer/static"
 MEDIA_ROOT = "/opt/services/CultureAnalyzer/media/"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432
+        'NAME': os.getenv('PG_DB'),
+        'USER': os.getenv('PG_USER'),
+        'PASSWORD': os.getenv('PG_PASSWORD'),
+        'HOST': os.getenv('PG_HOST'),
+        'PORT': os.getenv('PG_PORT_IN')
     }
 }
