446fd998f08a30cd0b08449a74cc436d172b5567
rostIvan <bbwgd77@gmail.com>
2019-04-07 00:33:40 +0300

    Updated fabfile, added execution deploy commands from fab

--- fabfile.py
+++ fabfile.py
@@ -1,33 +1,43 @@
-import os
+from fabric import Connection
 from fabric import task
-from paramiko import SSHClient, AutoAddPolicy
+from os import environ as env
 
-env = os.environ
+
+def echo_info(text, with_line=True):
+    line = '-' * 100 if with_line else ''
+    text = text + '\n' if text.startswith('$') else text
+    return f'echo -e "{line}\n{text}"'
+
+
+def command(text):
+    return echo_info(f'$ {text}') + '&&' + text
+
+
+DEV_DEPLOY_COMMANDS = (echo_info('Deploy...'),
+                       command('cd CultureAnalyzer-8080'),
+                       command('git checkout -f dev'),
+                       command('git pull'),
+                       command('docker-compose stop'),
+                       command('docker-compose up --build -d'),
+                       command('docker-compose ps'))
+
+USER = env['GC_DEPLOY_USERNAME']
+HOST = env['GC_DEPLOY_HOSTNAME']
+PORT = env['GC_DEPLOY_PORT']
+KEY_FILE = env['GC_DEPLOY_KEY_FILENAME']
 
 
 @task
-def deploy_gc(ctx):
-    deploy(username=env['GC_DEPLOY_USERNAME'],
-           hostname=env['GC_DEPLOY_HOSTNAME'],
-           port=env['GC_DEPLOY_PORT'],
-           key_filename=env['GC_DEPLOY_KEY_FILENAME'],
-           deploy_command=env['GC_DEPLOY_COMMAND'])
+def gc_deploy_dev(_ctx):
+    deploy(username=USER, hostname=HOST, port=PORT, key_filename=KEY_FILE,
+           deploy_commands=DEV_DEPLOY_COMMANDS)
 
 
-def deploy(username, hostname, port, key_filename, deploy_command):
-    print('-> Run fab deploy <-')
-    ssh_client = SSHClient()
-    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-    ssh_client.load_system_host_keys()
-    ssh_client.connect(username=username, hostname=hostname,
-                       port=port, key_filename=key_filename)
-    stdout, stderr = ssh_client.exec_command(deploy_command)[1:]
-    print(get_output(stderr, stdout))
-    ssh_client.close()
-    print('-> End deploy <-')
-
-
-def get_output(stderr, stdout):
-    out = stdout.read().decode("utf8")
-    err = stderr.read().decode("utf8")
-    return f'\nout: \n{out}\n\nerr: \n{err}'
+def deploy(username, hostname, port, key_filename, deploy_commands):
+    print('---> Run fab deploy <---')
+    connection = Connection(host=hostname, user=username, port=port,
+                            connect_kwargs={"key_filename": key_filename})
+    with connection as c:
+        result = c.run(' && '.join(deploy_commands))
+        print(result)
+    print('---> End deploy <---')
