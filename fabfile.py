553c6ba74d7c2f2b0f634d0a94c5ee05941e4777
rostIvan <bbwgd77@gmail.com>
2019-04-08 20:36:22 +0300

    Changed .travis, fabfile for checking autodeploy

--- fabfile.py
+++ fabfile.py
@@ -37,7 +37,7 @@ MASTER_DEPLOY_COMMANDS = (
 
 DEV_DEPLOY_COMMANDS = (
     *open_project(folder='CultureAnalyzer-8080'),
-    *update_project(branch='dev'),
+    *update_project(branch='dockerize/dev'),
     *update_docker(docker_compose_file='docker-compose.dev.yml')
 )
 
