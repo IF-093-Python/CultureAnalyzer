77314f9a644c6dc13c6417bbad11b7a1cce68167
rostIvan <bbwgd77@gmail.com>
2019-04-16 04:17:11 +0300

    Added 2 test where's clearing all or none sessions

--- users/tests/test_tasks.py
+++ users/tests/test_tasks.py
@@ -10,7 +10,7 @@ from users.tests.utils import mock_datetime_now, tz_datetime
 class TaskClearSessionsTest(DjangoTestCase):
     @mock_datetime_now("users.tasks.datetime",
                        year=2018, month=1, day=1)
-    def test_clear_expired_sessions(self):
+    def test_clear_expired_sessions_where_some_expired(self):
         old_and_expired = tz_datetime(year=2017, month=1, day=1)
         new_and_not_expired = tz_datetime(year=2018, month=2, day=1)
         self.generate_sessions(old_and_expired, limit=10)
@@ -18,12 +18,30 @@ class TaskClearSessionsTest(DjangoTestCase):
         clear_expired_sessions()
         self.assertIs(Session.objects.count(), 3)
 
-    def generate_sessions(self, initial_datetime,
-                          days_interval=1, minutes_interval=0, limit=5):
+    @mock_datetime_now("users.tasks.datetime",
+                       year=2019, month=1, day=1)
+    def test_clear_expired_sessions_where_all_expired(self):
+        all_expired = tz_datetime(year=2017, month=1, day=1)
+        self.generate_sessions(all_expired, limit=30)
+        clear_expired_sessions()
+        self.assertIs(Session.objects.count(), 0)
+
+    @mock_datetime_now("users.tasks.datetime",
+                       year=2019, month=3, day=10, hour=12)
+    def test_clear_expired_sessions_where_all_not_expired(self):
+        all_not_expired = tz_datetime(year=2019, month=3, day=10, hour=13)
+        self.generate_sessions(all_not_expired, limit=5,
+                               days_interval=0, hours_interval=2)
+        clear_expired_sessions()
+        self.assertIs(Session.objects.count(), 5)
+
+    def generate_sessions(self, initial_datetime, days_interval=1,
+                          minutes_interval=0, hours_interval=0, limit=5):
         expire_date = initial_datetime
         for i in range(0, limit):
             random_key = uuid.uuid4().hex[0:35]
             expire_date += timedelta(days=days_interval,
+                                     hours=hours_interval,
                                      minutes=minutes_interval)
             Session.objects.create(session_key=random_key,
                                    expire_date=expire_date)
